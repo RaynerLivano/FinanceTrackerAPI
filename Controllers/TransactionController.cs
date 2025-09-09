fa613a4789f3816ccc5f1197f31ba3cfd8737f6d
RaynerLivano <raynerlivanok@gmail.com>
2025-09-09 20:46:32 +0700

    Added Transaction model and controller

--- Controllers/TransactionController.cs
+++ Controllers/TransactionController.cs
@@ -8,6 +8,7 @@ namespace FinanceTrackerAPI.Controllers
     public class TransactionsController : ControllerBase
     {
         private static readonly List<Transaction> Transactions = new();
+        private static int Id = 1;
 
         [HttpGet]
         public IActionResult GetAll()
@@ -18,9 +19,26 @@ namespace FinanceTrackerAPI.Controllers
         [HttpPost]
         public IActionResult Create(Transaction transaction)
         {
-            transaction.Id = Transactions.Count + 1;
+            bool exits = Transactions.Any(t => 
+            t.Description == transaction.Description && 
+            t.Amount == transaction.Amount && 
+            t.Date.Date == transaction.Date.Date);
+
+            if (exits)
+            {
+                return Conflict("Transaction already exists.");
+            }
+            
+            transaction.Id = Id++;
             Transactions.Add(transaction);
             return CreatedAtAction(nameof(GetAll), transaction);
+        }
+
+        [HttpDelete("clear")]
+        public IActionResult Clear()
+        {
+            Transactions.Clear();
+            return NoContent();
         }
     }
 }
